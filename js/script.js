7c162f7e650fd17395ea42b132dc6ae2d346cd47
CaroGirGon <carolinagiraldogonzales@gmail.com>
2025-06-06 20:41:46 -0400

    First load

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+let slides = document.querySelectorAll('.home .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+};
+
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
